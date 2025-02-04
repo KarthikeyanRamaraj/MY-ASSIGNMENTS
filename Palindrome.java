1c1fc44cdd6539208787bc459c53ef7b0ef82519
KarthikeyanRamaraj <karthikeyanforjobs@gmail.com>
2025-02-04 12:32:52 +0530

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -1,22 +1,38 @@
 package Week1Assignments;
 
-import java.util.Scanner;
+
 
 public class Palindrome {
-    public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+    
+        int number = 12345;
+        
+     
+        if (isPalindrome(number)) {
+            System.out.println(number + " is a Palindrome.");
+        } else {
+            System.out.println(number + " is not a Palindrome.");
+        }
+    }
 
   
-        int input, output = 0;
-
-
-        System.out.print("Enter a number: ");
-        input = scanner.nextInt();
-
-
-        int originalInput = input;
-
+    public static boolean isPalindrome(int number) {
+        int originalNumber = number;
+        int reversedNumber = 0; 
+        int remainder;
+        
+     
+        while (number != 0) {
+            remainder = number % 10; 
+            reversedNumber = reversedNumber * 10 + remainder; 
+            number /= 10; 
+        }
+        
+        
+        return originalNumber == reversedNumber;
+    }
+}
         
         for ( ; input != 0; input /= 10) {
             int remainder = input % 10;
