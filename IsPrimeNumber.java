fc7743408774ce5244baf6f8ec36f86c2d068000
KarthikeyanRamaraj <karthikeyanforjobs@gmail.com>
2025-02-04 12:26:45 +0530

    Update IsPrimeNumber.java

--- IsPrimeNumber.java
+++ IsPrimeNumber.java
@@ -1,18 +1,35 @@
 package Week1Assignments;
 
-import java.util.Scanner;
-
 public class IsPrimeNumber {
 
     public static void main(String[] args) {
-    
-        Scanner scanner = new Scanner(System.in);
+
+        int number = 37; 
+        
+
+        if (isPrime(number)) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+    }
 
    
-        System.out.print("Enter a number: ");
-        int number = scanner.nextInt();
-
+    public static boolean isPrime(int number) {
      
+        if (number <= 1) {
+            return false;
+        }
+
+       
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true; 
+    }
+}
         boolean isPrime = true;
 
         if (number <= 1) {
